d84f2f0d36e19a25e0a213f712ed727cd40ed6b4
chamikara <chamikara98@gmail.com>
2021-02-12 20:44:17 +0530

    Added menuStrip created menus

--- Gym Membership Mangement System/Form1.Designer.cs
+++ Gym Membership Mangement System/Form1.Designer.cs
@@ -29,20 +29,115 @@ namespace Gym_Membership_Mangement_System
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newMemberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newStaffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.equipmentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchMemberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteMemberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.newMemberToolStripMenuItem,
+            this.newStaffToolStripMenuItem,
+            this.equipmentToolStripMenuItem,
+            this.searchMemberToolStripMenuItem,
+            this.deleteMemberToolStripMenuItem,
+            this.logOutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(889, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(33, 24);
+            this.toolStripMenuItem1.Text = ">";
+            // 
+            // newMemberToolStripMenuItem
+            // 
+            this.newMemberToolStripMenuItem.Name = "newMemberToolStripMenuItem";
+            this.newMemberToolStripMenuItem.Size = new System.Drawing.Size(113, 24);
+            this.newMemberToolStripMenuItem.Text = "New Member";
+            // 
+            // newStaffToolStripMenuItem
+            // 
+            this.newStaffToolStripMenuItem.Name = "newStaffToolStripMenuItem";
+            this.newStaffToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.newStaffToolStripMenuItem.Text = "New Staff";
+            // 
+            // equipmentToolStripMenuItem
+            // 
+            this.equipmentToolStripMenuItem.Name = "equipmentToolStripMenuItem";
+            this.equipmentToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
+            this.equipmentToolStripMenuItem.Text = "Equipment";
+            // 
+            // searchMemberToolStripMenuItem
+            // 
+            this.searchMemberToolStripMenuItem.Name = "searchMemberToolStripMenuItem";
+            this.searchMemberToolStripMenuItem.Size = new System.Drawing.Size(127, 24);
+            this.searchMemberToolStripMenuItem.Text = "Search Member";
+            // 
+            // deleteMemberToolStripMenuItem
+            // 
+            this.deleteMemberToolStripMenuItem.Name = "deleteMemberToolStripMenuItem";
+            this.deleteMemberToolStripMenuItem.Size = new System.Drawing.Size(127, 24);
+            this.deleteMemberToolStripMenuItem.Text = "Delete Member";
+            // 
+            // logOutToolStripMenuItem
+            // 
+            this.logOutToolStripMenuItem.Name = "logOutToolStripMenuItem";
+            this.logOutToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.logOutToolStripMenuItem.Text = "Log Out";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(889, 508);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Gym Membership Management System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newMemberToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newStaffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem equipmentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchMemberToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteMemberToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logOutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
