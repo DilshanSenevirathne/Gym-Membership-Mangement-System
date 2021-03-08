d243210b619f485b7796e1863558c37735957319
chamikara <chamikara98@gmail.com>
2021-03-08 21:13:46 +0530

    Added compnents to the new member.cs and coded to get inputs

--- Gym Membership Mangement System/New Member.Designer.cs
+++ Gym Membership Mangement System/New Member.Designer.cs
@@ -30,7 +30,289 @@ namespace Gym_Membership_Mangement_System
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(New_Member));
+            this.inputFirstName = new System.Windows.Forms.TextBox();
+            this.inputLastName = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.dateTimePickerDOB = new System.Windows.Forms.DateTimePicker();
+            this.inputEmail = new System.Windows.Forms.TextBox();
+            this.inputMobile = new System.Windows.Forms.TextBox();
+            this.dateTimePickerJoinDate = new System.Windows.Forms.DateTimePicker();
+            this.inputAddress = new System.Windows.Forms.RichTextBox();
+            this.comboBoxGymTime = new System.Windows.Forms.ComboBox();
+            this.comboBoxMembership = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // inputFirstName
+            // 
+            this.inputFirstName.Location = new System.Drawing.Point(175, 92);
+            this.inputFirstName.Name = "inputFirstName";
+            this.inputFirstName.Size = new System.Drawing.Size(100, 22);
+            this.inputFirstName.TabIndex = 0;
+            this.inputFirstName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // inputLastName
+            // 
+            this.inputLastName.Location = new System.Drawing.Point(175, 155);
+            this.inputLastName.Name = "inputLastName";
+            this.inputLastName.Size = new System.Drawing.Size(100, 22);
+            this.inputLastName.TabIndex = 1;
+            this.inputLastName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.ForeColor = System.Drawing.Color.White;
+            this.radioButton1.Location = new System.Drawing.Point(129, 212);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 24);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Male";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.ForeColor = System.Drawing.Color.White;
+            this.radioButton2.Location = new System.Drawing.Point(212, 212);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 24);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // dateTimePickerDOB
+            // 
+            this.dateTimePickerDOB.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerDOB.Location = new System.Drawing.Point(203, 292);
+            this.dateTimePickerDOB.Name = "dateTimePickerDOB";
+            this.dateTimePickerDOB.Size = new System.Drawing.Size(100, 22);
+            this.dateTimePickerDOB.TabIndex = 4;
+            this.dateTimePickerDOB.ValueChanged += new System.EventHandler(this.dateTimePickerDOB_ValueChanged);
+            // 
+            // inputEmail
+            // 
+            this.inputEmail.Location = new System.Drawing.Point(467, 95);
+            this.inputEmail.Name = "inputEmail";
+            this.inputEmail.Size = new System.Drawing.Size(100, 22);
+            this.inputEmail.TabIndex = 5;
+            // 
+            // inputMobile
+            // 
+            this.inputMobile.Location = new System.Drawing.Point(175, 352);
+            this.inputMobile.Name = "inputMobile";
+            this.inputMobile.Size = new System.Drawing.Size(100, 22);
+            this.inputMobile.TabIndex = 6;
+            // 
+            // dateTimePickerJoinDate
+            // 
+            this.dateTimePickerJoinDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerJoinDate.Location = new System.Drawing.Point(467, 151);
+            this.dateTimePickerJoinDate.Name = "dateTimePickerJoinDate";
+            this.dateTimePickerJoinDate.Size = new System.Drawing.Size(100, 22);
+            this.dateTimePickerJoinDate.TabIndex = 7;
+            // 
+            // inputAddress
+            // 
+            this.inputAddress.Location = new System.Drawing.Point(432, 264);
+            this.inputAddress.Name = "inputAddress";
+            this.inputAddress.Size = new System.Drawing.Size(100, 96);
+            this.inputAddress.TabIndex = 9;
+            this.inputAddress.Text = "";
+            // 
+            // comboBoxGymTime
+            // 
+            this.comboBoxGymTime.FormattingEnabled = true;
+            this.comboBoxGymTime.Items.AddRange(new object[] {
+            "06AM  - 07AM",
+            "07AM  - 08AM",
+            "09AM  - 10AM",
+            "05PM  -  06PM",
+            "06PM  -  07PM"});
+            this.comboBoxGymTime.Location = new System.Drawing.Point(467, 205);
+            this.comboBoxGymTime.Name = "comboBoxGymTime";
+            this.comboBoxGymTime.Size = new System.Drawing.Size(100, 24);
+            this.comboBoxGymTime.TabIndex = 10;
+            // 
+            // comboBoxMembership
+            // 
+            this.comboBoxMembership.FormattingEnabled = true;
+            this.comboBoxMembership.Items.AddRange(new object[] {
+            " 01   Months\t",
+            " 02   Months",
+            " 03   Months",
+            " 04   Months",
+            " 06   Months",
+            " 12   Months"});
+            this.comboBoxMembership.Location = new System.Drawing.Point(376, 393);
+            this.comboBoxMembership.Name = "comboBoxMembership";
+            this.comboBoxMembership.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxMembership.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(5, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 35);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "First Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(12, 352);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 31);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Mobile";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Location = new System.Drawing.Point(10, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 37);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Date Of Birth";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(22, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label5.Location = new System.Drawing.Point(5, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 39);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Last Name";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label6.Location = new System.Drawing.Point(170, 393);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(209, 25);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Membership Time";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(148, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 31);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(319, 441);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 31);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Image = ((System.Drawing.Image)(resources.GetObject("label7.Image")));
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label7.Location = new System.Drawing.Point(309, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(158, 32);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Gym Time";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Image = ((System.Drawing.Image)(resources.GetObject("label8.Image")));
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label8.Location = new System.Drawing.Point(314, 149);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(147, 28);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Join Date";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Image = ((System.Drawing.Image)(resources.GetObject("label9.Image")));
+            this.label9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label9.Location = new System.Drawing.Point(309, 86);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(111, 34);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Email";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // New_Member
             // 
@@ -38,14 +320,63 @@ namespace Gym_Membership_Mangement_System
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(630, 484);
+            this.ClientSize = new System.Drawing.Size(607, 484);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxMembership);
+            this.Controls.Add(this.comboBoxGymTime);
+            this.Controls.Add(this.inputAddress);
+            this.Controls.Add(this.dateTimePickerJoinDate);
+            this.Controls.Add(this.inputMobile);
+            this.Controls.Add(this.inputEmail);
+            this.Controls.Add(this.dateTimePickerDOB);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.inputLastName);
+            this.Controls.Add(this.inputFirstName);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(625, 531);
             this.Name = "New_Member";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "New_Member";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox inputFirstName;
+        private System.Windows.Forms.TextBox inputLastName;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDOB;
+        private System.Windows.Forms.TextBox inputEmail;
+        private System.Windows.Forms.TextBox inputMobile;
+        private System.Windows.Forms.DateTimePicker dateTimePickerJoinDate;
+        private System.Windows.Forms.RichTextBox inputAddress;
+        private System.Windows.Forms.ComboBox comboBoxGymTime;
+        private System.Windows.Forms.ComboBox comboBoxMembership;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
--- Gym Membership Mangement System/New Member.cs
+++ Gym Membership Mangement System/New Member.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,59 @@ namespace Gym_Membership_Mangement_System
         public New_Member()
         {
             InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstName = inputFirstName.Text;
+            string lastName = inputLastName.Text;
+
+            string gender = "";
+
+            bool isChecked = radioButton1.Checked;
+
+            if (isChecked)
+            {
+                gender = radioButton1.Text;
+            }
+
+            else
+            {
+                gender = radioButton2.Text;
+            }
+
+            string dob = dateTimePickerDOB.Text;
+            Int64 mobile = Int64.Parse(inputMobile.Text);
+            string email = inputEmail.Text;
+            string joinDate = dateTimePickerJoinDate.Text;
+            string gymTime = comboBoxGymTime.Text;
+            string address = inputAddress.Text;
+            string membershipTime = comboBoxMembership.Text;
+
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source = CHAMIKARA\\SQLEXPRESS; databse =gym; integrated security = True";
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePickerDOB_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
