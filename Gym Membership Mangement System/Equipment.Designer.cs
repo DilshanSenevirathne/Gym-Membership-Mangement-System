2b8d70e9fd6640475ba3642841f6eaed8e7f35d9
chamikara <chamikara98@gmail.com>
2021-04-20 18:53:39 +0530

    changed appearence of Equipment Form

--- Gym Membership Mangement System/Equipment.Designer.cs
+++ Gym Membership Mangement System/Equipment.Designer.cs
@@ -30,11 +30,6 @@ namespace Gym_Membership_Mangement_System
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Equipment));
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.txtMuscleUsed = new System.Windows.Forms.TextBox();
             this.txtCost = new System.Windows.Forms.TextBox();
             this.txtEquipName = new System.Windows.Forms.TextBox();
@@ -43,104 +38,53 @@ namespace Gym_Membership_Mangement_System
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.dateTimePickerDeliveryDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(48, 404);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 25);
-            this.label1.TabIndex = 23;
-            this.label1.Text = "Cost";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(48, 341);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 25);
-            this.label2.TabIndex = 24;
-            this.label2.Text = "Delivery Date";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(48, 279);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 25);
-            this.label3.TabIndex = 25;
-            this.label3.Text = "Muscles used";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(48, 158);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(120, 25);
-            this.label5.TabIndex = 26;
-            this.label5.Text = "Description";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(48, 95);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(176, 25);
-            this.label6.TabIndex = 27;
-            this.label6.Text = "Equipment Name";
             // 
             // txtMuscleUsed
             // 
-            this.txtMuscleUsed.Location = new System.Drawing.Point(253, 279);
+            this.txtMuscleUsed.Location = new System.Drawing.Point(543, 357);
             this.txtMuscleUsed.Name = "txtMuscleUsed";
-            this.txtMuscleUsed.Size = new System.Drawing.Size(195, 22);
+            this.txtMuscleUsed.Size = new System.Drawing.Size(261, 22);
             this.txtMuscleUsed.TabIndex = 2;
             // 
             // txtCost
             // 
-            this.txtCost.Location = new System.Drawing.Point(253, 404);
+            this.txtCost.Location = new System.Drawing.Point(543, 486);
             this.txtCost.Name = "txtCost";
-            this.txtCost.Size = new System.Drawing.Size(195, 22);
+            this.txtCost.Size = new System.Drawing.Size(261, 22);
             this.txtCost.TabIndex = 4;
             // 
             // txtEquipName
             // 
-            this.txtEquipName.Location = new System.Drawing.Point(253, 99);
+            this.txtEquipName.Location = new System.Drawing.Point(543, 168);
             this.txtEquipName.Name = "txtEquipName";
-            this.txtEquipName.Size = new System.Drawing.Size(195, 22);
+            this.txtEquipName.Size = new System.Drawing.Size(261, 22);
             this.txtEquipName.TabIndex = 0;
             // 
             // txtDescription
             // 
-            this.txtDescription.Location = new System.Drawing.Point(253, 145);
+            this.txtDescription.Location = new System.Drawing.Point(543, 237);
             this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(270, 96);
+            this.txtDescription.Size = new System.Drawing.Size(261, 96);
             this.txtDescription.TabIndex = 1;
             this.txtDescription.Text = "";
             // 
             // button2
             // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(253, 441);
+            this.button2.Location = new System.Drawing.Point(380, 576);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 31);
+            this.button2.Size = new System.Drawing.Size(192, 62);
             this.button2.TabIndex = 6;
             this.button2.Text = "Reset";
             this.button2.UseVisualStyleBackColor = true;
@@ -148,11 +92,14 @@ namespace Gym_Membership_Mangement_System
             // 
             // button1
             // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(122, 441);
+            this.button1.Location = new System.Drawing.Point(119, 576);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 31);
+            this.button1.Size = new System.Drawing.Size(192, 62);
             this.button1.TabIndex = 5;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
@@ -160,10 +107,12 @@ namespace Gym_Membership_Mangement_System
             // 
             // button3
             // 
-            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(388, 441);
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button3.Location = new System.Drawing.Point(637, 576);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(164, 31);
+            this.button3.Size = new System.Drawing.Size(207, 62);
             this.button3.TabIndex = 7;
             this.button3.Text = "View Equipment";
             this.button3.UseVisualStyleBackColor = true;
@@ -172,17 +121,108 @@ namespace Gym_Membership_Mangement_System
             // dateTimePickerDeliveryDate
             // 
             this.dateTimePickerDeliveryDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePickerDeliveryDate.Location = new System.Drawing.Point(253, 344);
+            this.dateTimePickerDeliveryDate.Location = new System.Drawing.Point(543, 416);
             this.dateTimePickerDeliveryDate.Name = "dateTimePickerDeliveryDate";
-            this.dateTimePickerDeliveryDate.Size = new System.Drawing.Size(195, 22);
+            this.dateTimePickerDeliveryDate.Size = new System.Drawing.Size(261, 22);
             this.dateTimePickerDeliveryDate.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Location = new System.Drawing.Point(145, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(253, 35);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Equipment Name";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Image = ((System.Drawing.Image)(resources.GetObject("label7.Image")));
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label7.Location = new System.Drawing.Point(145, 237);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(196, 35);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Description";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Image = ((System.Drawing.Image)(resources.GetObject("label8.Image")));
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label8.Location = new System.Drawing.Point(145, 348);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(229, 35);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "Muscles Used";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Image = ((System.Drawing.Image)(resources.GetObject("label9.Image")));
+            this.label9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label9.Location = new System.Drawing.Point(145, 409);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(221, 35);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Delivery Date";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Image = ((System.Drawing.Image)(resources.GetObject("label10.Image")));
+            this.label10.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label10.Location = new System.Drawing.Point(145, 477);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(138, 35);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "Cost";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Brush Script MT", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Image = ((System.Drawing.Image)(resources.GetObject("label11.Image")));
+            this.label11.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label11.Location = new System.Drawing.Point(198, 29);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(525, 96);
+            this.label11.TabIndex = 35;
+            this.label11.Text = "Southern Fitness";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // Equipment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(607, 484);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(948, 675);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.dateTimePickerDeliveryDate);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -191,11 +231,7 @@ namespace Gym_Membership_Mangement_System
             this.Controls.Add(this.txtEquipName);
             this.Controls.Add(this.txtCost);
             this.Controls.Add(this.txtMuscleUsed);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
             this.Name = "Equipment";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Equipment";
@@ -206,12 +242,6 @@ namespace Gym_Membership_Mangement_System
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtMuscleUsed;
         private System.Windows.Forms.TextBox txtCost;
         private System.Windows.Forms.TextBox txtEquipName;
@@ -220,5 +250,11 @@ namespace Gym_Membership_Mangement_System
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DateTimePicker dateTimePickerDeliveryDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
--- Gym Membership Mangement System/Program.cs
+++ Gym Membership Mangement System/Program.cs
@@ -16,7 +16,7 @@ namespace Gym_Membership_Mangement_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new NewStaff());
+            Application.Run(new Equipment());
         }
     }
 }
